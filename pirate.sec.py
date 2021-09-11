c96aa9f846bf3b1d30828f11fd69bc4615ffe238
P1ratesec <90520934+P1ratesec@users.noreply.github.com>
2021-09-11 17:48:29 +0100

    Update pirate.sec.py

--- pirate.sec.py
+++ pirate.sec.py
@@ -42,10 +42,10 @@ def ddos():
                                     /   )
                                    /   /  
                                   /-. /
-                    (_)"-._     /   )
-                       "-._ "-'""( )/    
-                         "-/"-._" `. 
-                           /     "-.'._
+                    (_)"-._     /   )     Give me your port's.
+                       "-._ "-'""( )/           /
+                         "-/"-._" `.           /
+                           /     "-.'._       /
                            /\       /-._"-._
             _,---...__    /  ) _,-"/    "-(_)
         ___<__(|) _   ""-/  / /   /
@@ -68,12 +68,17 @@ def ddos():
  |    |   |  ||  | \// __ \|  | \  ___/      \___ \\  ___/\  \___ 
  |____|   |__||__|  (____  /__|  \___  > /\ /____  >\___  >\___  >
                          \/          \/  \/      \/     \/     \/ 
-    Distributed Denial of Service Attack 
-   
-
+    Distributed Denial of Service Attack by Pirate.Sec     
+         
    '''+Style.RESET_ALL+Fore.GREEN+Style.BRIGHT+''' 
  Shall we down a website today sir? :3 
-             
+ ------------------------------------------------------------------------
+ Warning do not use this tool if you have epilepsy or hate bright colour's 
+ ------------------------------------------------------------------------     
+ instagram acc :) https://www.instagram.com/pirate_sec/        
+ ------------------
+ beta version 1.0 
+ ------------------
         '''+Style.RESET_ALL)
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -90,7 +95,7 @@ def ddos():
             exit()
  
     try:
-        print(Fore.CYAN+Style.BRIGHT+"Checking internet connection ... "+Fore.RESET)
+        print(Fore.CYAN+Style.BRIGHT+"𝙒𝙚𝙡𝙘𝙤𝙢𝙚 𝙡𝙤𝙖𝙙𝙞𝙣𝙜 𝙮𝙤𝙪 𝙞𝙣.......... "+Fore.RESET)
         for i in tqdm(range(30000)):
             print(end=Fore.MAGENTA+Style.BRIGHT+'\r')
 
@@ -106,7 +111,7 @@ def ddos():
             print(Fore.BLACK+Style.BRIGHT+"1."+Style.RESET_ALL+Fore.BLUE+" website domain"+Fore.BLACK+Style.BRIGHT+"\n2."+Style.RESET_ALL+Fore.RED+" IP adress"+Fore.BLACK+Style.BRIGHT+"\n3."+Style.RESET_ALL+Fore.GREEN+" leave :(")
             opt=str(input(Fore.RED+Style.BRIGHT+"\n>>> "+Fore.RESET))
             if opt=='1':
-                domain=str(input(Fore.CYAN+Style.BRIGHT+"type in your target site (ex: google.com)"+Fore.RESET))
+                domain=str(input(Fore.CYAN+Style.BRIGHT+"type in your target site (ex: google.com):"+Fore.RESET))
                 ip=socket.gethostbyname(domain)
                 break
             elif opt=='2':
@@ -115,6 +120,7 @@ def ddos():
             elif opt=='3':
                 time.sleep(1)
                 print(Fore.RED+"that was fun ( ಠ ͜ʖಠ)"+Fore.RESET)
+                                                             
                 exit()
             else:
                 print(Fore.RED+'the fuck u doing lol thats not a option'+Fore.RESET)
@@ -132,7 +138,33 @@ def ddos():
         time.sleep(1)
         sent = 0
     except Exception as e:
-        print(Fore.RED+"error 404 something went wrong")
+        print(Fore.RED+'''
+              ,---------------------------,
+              |  /---------------------\  |
+              | |                       | |
+              | |     ERROR 404         | |
+              | |   (╯°□°）╯︵ ┻━┻       | |    error this might help http://www.dolekemp96.org/main.htm 
+              | |                       | |          /
+              | |                       | |         / 
+              |  \_____________________/  |        /
+              |___________________________|       /
+            ,---\_____     []     _______/------,
+          /         /______________\           /|
+        /___________________________________ /  | ___
+        |                                   |   |    )
+        |  _ _ _                 [-------]  |   |   (
+        |  o o o                 [-------]  |  /    _)_
+        |__________________________________ |/     /  /
+    /-------------------------------------/|      ( )/
+  /-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/ /
+/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/ /
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+      
+           
+                  
+    
+                
+''')
         print("Reason: ",e,Fore.RESET)
         time.sleep(3)
         ddos()
@@ -150,53 +182,46 @@ def ddos():
             elif port==1900:
                 port=1901
     except Exception as e:
-        print(Fore.RED+Style.BRIGHT+"Finished \ nReason: ",e,Fore.RESET)
+        print(Fore.RED+Style.BRIGHT+'''       
+       __                                        has it been done master?
+      <  \                                            /
+[\\\\\\(\ (:::<======================================-
+\<      >  \
+ \\    /    |
+  `==='____/
+
+
+''',e,Fore.RESET)
         time.sleep(3)
         ddos()
     except KeyboardInterrupt:
-         print(Fore.BLUE+'''
-                                                      
-                                        ,-.
-                                       ("O_)
-                                      / `-/
-                                     /-. /
-                                    /   )
-                                   /   /  
-                                  /-. /
-                    (_)"-._     /   )
-                       "-._ "-'""( )/    
-                         "-/"-._" `. 
-                           /     "-.'._
-                           /\       /-._"-._
-            _,---...__    /  ) _,-"/    "-(_)
-        ___<__(|) _   ""-/  / /   /
-         '  `----' ""-.   \/ /   /
-                       )  ] /   /
-               ____..-'   //   /                       )
-           ,-""      __.,'/   /   ___                 /,
-          /    ,--""/  / /   /,-""   """-.          ,'/
-         [    (    /  / /   /  ,.---,_   `._   _,-','
-          \    `-./  / /   /  /       `-._  """ ,-'
-           `-._  /  / /   /_,'            ""--"
-               "/  / /   /"         
-               /  / /   /
-              /  / /   /       "Cringe security? tut tut tut destroy it"
-             /  |,'   /  
-            :   /    /     
- ________.__   /    /       __                                  
-\______   \__|___________ _/  |_  ____        ______ ____   ____  
- |     ___/  \_  __ \__  \\   __\/ __ \      /  ___// __ \_/ ___\ 
- |    |   |  ||  | \// __ \|  | \  ___/      \___ \\  ___/\  \___ 
- |____|   |__||__|  (____  /__|  \___  > /\ /____  >\___  >\___  >
-                         \/          \/  \/      \/     \/     \/ 
-    Distributed Denial of Service Attack 
-   
-
+       print(Fore.RED+'''
+Attack quit by user. 
    '''+Style.RESET_ALL+Fore.GREEN+Style.BRIGHT+''' 
- Shall we down a website today sir? :3 
-             
+ _______  _______  _______  ______   _______  __   __  _______   
+|       ||       ||       ||      | |  _    ||  | |  ||       |  
+|    ___||   _   ||   _   ||  _    || |_|   ||  |_|  ||    ___|  
+|   | __ |  | |  ||  | |  || | |   ||       ||       ||   |___   
+|   ||  ||  |_|  ||  |_|  || |_|   ||  _   | |_     _||    ___|  
+|   |_| ||       ||       ||       || |_|   |  |   |  |   |___   
+|_______||_______||_______||______| |_______|  |___|  |_______|  
+          ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
+        ▄▀░░░░░░░░░░░░▄░░░░░░░▀▄
+        █░░▄░░░░▄░░░░░░░░░░░░░░█
+        █░░░░░░░░░░░░▄█▄▄░░▄░░░█ ▄▄▄
+ ▄▄▄▄▄  █░░░░░░▀░░░░▀█░░▀▄░░░░░█▀▀░██
+ ██▄▀██▄█░░░▄░░░░░░░██░░░░▀▀▀▀▀░░░░██
+  ▀██▄▀██░░░░░░░░▀░██▀░░░░░░░░░░░░░▀██
+    ▀████░▀░░░░▄░░░██░░░▄█░░░░▄░▄█░░██
+       ▀█░░░░▄░░░░░██░░░░▄░░░▄░░▄░░░██
+       ▄█▄░░░░░░░░░░░▀▄░░▀▀▀▀▀▀▀▀░░▄▀
+      █▀▀█████████▀▀▀▀████████████▀
+       ████▀  ███▀      ▀███  ▀██▀
+         
         '''+Style.RESET_ALL)
+                                                      
 
+   
 
 
 ddos()
