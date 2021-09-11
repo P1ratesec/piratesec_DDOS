4fcf69dfabcdf02d6e032abae7b39884a3ff6b74
P1ratesec <90520934+P1ratesec@users.noreply.github.com>
2021-09-11 17:42:22 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ echo "2) Exit"
 read -p ">> " resp
 if [ "$resp" == "$var1" ]
 then
-python3 biyivi_ddos.py
+python biyivi_ddos.py
 else
 echo "to start the script you must write: python3 piratesec_DDOS.py "
 echo ":D"
