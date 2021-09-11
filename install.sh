54075088fafa4d00f0941cbbe0dca4dcf0549d92
P1ratesec <90520934+P1ratesec@users.noreply.github.com>
2021-09-11 17:30:29 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,21 @@
+echo "starting installation ..."
+sleep 2 
+pkg install python -y 
+clear
+pip install colorama 
+clear
+pip install tqdm 
+clear
+echo "The installation is complete."
+var1="1"
+echo "done script"
+echo "1) Yes"
+echo "2) Exit"
+read -p ">> " resp
+if [ "$resp" == "$var1" ]
+then
+python3 biyivi_ddos.py
+else
+echo "to start the script you must write: python3 piratesec_DDOS.py "
+echo ":D"
+fi 
